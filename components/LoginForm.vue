5ce396ea5aa11fa8e5bc3db3cffa6517d2c2e62d
Jhose Portillo <jhosmar26.00@gmail.com>
2022-03-23 12:46:17 -0500

    delete action adn rename file to 'tasks'

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -43,7 +43,7 @@ export default {
   methods: {
     login() {
       if(this.validateCredentials()) {
-        this.$router.push("/tareas")
+        this.$router.push("/tasks")
       }
     },
     validateCredentials() {
---
+++ pages/tasks.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="h-screen flex">
+    <section class="flex w-full bg-gradient-to-tr from-blue-800 to-purple-700 justify-center py-10">
+      <div class="bg-white pt-3.5 px-8 pb-8 rounded-xl w-full max-w-screen-lg">
+        <div>
+          <h1 class="text-gray-800 font-bold text-2xl uppercase">Lista de tareas</h1>
+          <!-- <label> -->
+            <span>Tarea:</span>
+            <input pattern="[a-zA-Z0-9]+" v-model="toAdd" class="border" type="text" />
+            <button type="submit" @click.prevent="addTodo">send</button>
+          <!-- </label> -->
+          <ul>
+            <li v-for="tarea, i in tasks" :key="i">
+              <input type="checkbox" :value="i" v-model="checked">
+              {{tarea}}
+              <input v-if="checked.indexOf(i) === -1" type="button" @click="() => deleteTask(i)" value="x">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'tasks',
+    data() {
+      return {
+        toAdd: "",
+        tasks: ["hacer la comida", "tasks"],
+        checked:[]
+      }
+    },
+    methods: {
+      addTodo() {
+        if(this.toAdd.match(/[a-zA-Z0-9]+/)){
+          this.tasks.push(this.toAdd);
+          this.toAdd = "";
+        } else {
+          alert("Solo se acepta valores alfanuméricos")
+        }
+      },
+      deleteTask(taskIndex) {
+        this.tasks.splice(taskIndex,1);
+        // this.checked = this.checked.filter((e)=>{ return e !== taskIndex })
+      }
+    }
+  }
+</script>
