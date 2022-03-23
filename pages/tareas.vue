937b036a783b0696b4a184f8583adbfde81e1b2f
Jhose Portillo <jhosmar26.00@gmail.com>
2022-03-23 12:01:10 -0500

    tareas

--- pages/tareas.vue
+++ pages/tareas.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="h-screen flex">
-    <section class="flex w-full bg-gradient-to-tr from-blue-800 to-purple-700">
-      <div>
-        <div class="uppercase">Lista de tareas</div>
-        <div class="">
-          s
+    <section class="flex w-full bg-gradient-to-tr from-blue-800 to-purple-700 justify-center py-10">
+      <div class="bg-white pt-3.5 px-8 pb-8 rounded-xl w-full max-w-screen-lg">
+        <div>
+          <h1 class="text-gray-800 font-bold text-2xl uppercase">Lista de tareas</h1>
+          <label>
+            <span>Tarea:</span>
+            <input :v-model="toAdd" class="border" type="text">
+            {{toAdd}}
+            <button type="submit" @click.prevent="()=>addTodo(toAdd)">send</button>
+          </label>
+          <ul>
+            <li v-for="tarea, i in tareas" :key="i">
+              <input type="checkbox" :value="i" v-model="checked">
+              {{tarea}}
+              <!-- <input v-if="" type="button" value="x"> -->
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -15,5 +27,17 @@
 <script>
   export default {
     name: 'tareas',
+    data() {
+      return {
+        toAdd: "",
+        tareas: ["hacer la comida", "tareas"],
+        checked:[]
+      }
+    },
+    methods: {
+      addTodo(value) {
+        this.tareas.push(value)
+      }
+    }
   }
 </script>
